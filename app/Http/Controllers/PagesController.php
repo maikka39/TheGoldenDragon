4da440b3e4801591dd8a775984e05cbf027dfdb6
Maik de Kruif <maik@maik.dev>
2021-04-12 14:46:56 +0200

    Setup laravel with vue

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke()
+    {
+        return view('welcome');
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import 'bootstrap'
+
+//Main pages
+import App from './components/app.vue'
+
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});

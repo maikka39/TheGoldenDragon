6a3d50241860fbc0a6058d2330c5444879e14614
Maik de Kruif <maik@maik.dev>
2021-06-18 21:45:20 +0200

    Add news text

---
+++ resources/js/components/home/pages/news.vue
@@ -0,0 +1,6 @@
+<template>
+    <p>
+        Door de Corona crisis is De Gouden Draak op het moment slechts beperkt open.<br>
+        Het restaurant-gedeelte is gesloten. U kan uw favoriete gerechten nog wel afhalen.
+    </p>
+</template>

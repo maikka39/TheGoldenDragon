62d96848486ac8c6f2dc770592b3ab8e172d65b5
Maik de Kruif <maik@maik.dev>
2021-06-25 16:08:21 +0200

    Remove console.log

--- resources/js/components/home/pages/menu/menu_items.vue
+++ resources/js/components/home/pages/menu/menu_items.vue
@@ -35,11 +35,8 @@ export default {
     fetch("/api/menuitems")
       .then(response => response.json())
       .then(data => (this.menu_items = data.data.sort((e1, e2) => {
-          console.log(e1.number > e2.number);
           return e1.number > e2.number
-      }))).then(() => {
-          console.log(this.menu_items);
-      })
+      })))
   },
   components: { MenuItem }
 }

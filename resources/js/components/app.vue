04a33df3db1774a30ebf57420c4cfa7f292865b4
Maik de Kruif <maik@maik.dev>
2021-06-27 15:17:41 +0200

    Remove main nav as it's not used

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="content">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link v-if="isLoggedIn" to="/cash">Cash</router-link>
-      <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
-      <button type="button" @click="logout()" v-if="isLoggedIn">
-        Logout
-      </button>
-    </div>
-    <div id="page">
       <router-view/>
-    </div>
   </div>
 </template>
 
@@ -18,33 +8,5 @@
 #content {
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-
-  #page {
-    flex: 1;
-  }
-}
-
-#nav {
-  display: none
 }
 </style>
-
-<script>
-import { mapGetters } from 'vuex'
-
-export default {
-  computed: {
-    ...mapGetters([
-      'isLoggedIn'
-    ])
-  },
-
-  methods: {
-    logout () {
-      this.$store.dispatch('logout')
-    }
-  }
-}
-</script>
